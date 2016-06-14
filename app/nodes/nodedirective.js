02047ba8e2d79a9bd0cbfe45a79b529cf8ce1560
vishal-j <vishal.jain@gmail.com>
2016-06-14 15:52:10 -0700

    showing node provisioning animation

---
+++ app/nodes/nodedirective.js
@@ -0,0 +1,25 @@
+/**
+ * Created by vjain3 on 6/14/16.
+ */
+angular.module("contiv.nodes")
+    .directive("ctvNodestatus", function() {
+        return {
+            restrict: 'E',
+            scope: {
+                node: '='
+            },
+            link: function (scope) {
+
+            },
+            templateUrl: 'nodes/nodestatus.html'
+        }
+    })
+    .directive("ctvNodestate", function() {
+        return {
+            restrict: 'E',
+            scope: {
+                node: '='
+            },
+            templateUrl: 'nodes/nodestate.html'
+        }
+    });
