c68660d4bccc3693d763c8dae427a7d64d217771
Melanie Tom <melanieetom@gmail.com>
2016-06-25 00:21:34 -0700

    format fixes

--- app/global_settings/module.js
+++ app/global_settings/module.js
@@ -1,36 +1,36 @@
 angular.module('contiv.globalsettings', ['contiv.models', 'contiv.directives', 'contiv.utils'])
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-	        .state('contiv.menu.globalsettings', {
-	            url: '/global',
-	            abstract: true,
-	            template: '<div ui-view class="ui container"/>'
-	        })
-	        .state('contiv.menu.globalsettings.settings', {
+            .state('contiv.menu.globalsettings', {
+                url: '/global',
+                abstract: true,
+                template: '<div ui-view class="ui container"/>'
+            })
+            .state('contiv.menu.globalsettings.settings', {
                 url: '/settings',
                 templateUrl: 'global_settings/globalsmenu.html'
             })
-	        .state('contiv.menu.globalsettings.settings.logs', {
+            .state('contiv.menu.globalsettings.settings.logs', {
                 url: '/logs',
                 controller: '',
                 templateUrl: ''
-	        })
-	        .state('contiv.menu.globalsettings.settings.auth', {
-	            url: '/auth',
-	            controller: '',
-	            templateUrl: ''
-	        })
-	        .state('contiv.menu.globalsettings.settings.license', {
-	            url: '/license',
-	            controller: '',
-	            templateUrl: ''
-	        })
-	        .state('contiv.menu.globalsettings.settings.networks', {
+            })
+            .state('contiv.menu.globalsettings.settings.auth', {
+                url: '/auth',
+                controller: '',
+                templateUrl: ''
+            })
+            .state('contiv.menu.globalsettings.settings.license', {
+                url: '/license',
+                controller: '',
+                templateUrl: ''
+            })
+            .state('contiv.menu.globalsettings.settings.networks', {
                 url: '/networks',
                 controller: '',
                 templateUrl: ''
             })
-	        .state('contiv.menu.globalsettings.settings.policies', {
+            .state('contiv.menu.globalsettings.settings.policies', {
                 url: '/policies',
                 controller: '',
                 templateUrl: ''
