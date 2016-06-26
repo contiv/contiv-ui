988307ccde3bf8a248e00fbac789ba34bf57964e
Melanie Tom <melanieetom@gmail.com>
2016-06-25 23:17:55 -0700

    Directive for active and last logs added

---
+++ app/components/directives/logsdirective.js
@@ -0,0 +1,12 @@
+angular.module("contiv.directives")
+    .directive("ctvLogs", function () {
+        return {
+            restrict: 'EA',
+            templateUrl: 'components/directives/logs.html',
+            scope: {
+				log: "=",
+				title: "@",
+				modalid: "@"
+            }
+        };
+    });
