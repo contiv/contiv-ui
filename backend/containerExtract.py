d6c621edef753203e51f613cf1e6958b871ccba1
Rohan Sikdar <rsikdar@berkeley.edu>
2016-08-09 14:40:43 +0530

    backend script to generate telegraf config file

--- backend/containerExtract.py
+++ backend/containerExtract.py
@@ -1,14 +1,19 @@
-#python script for extracting containers not part of a service
+#Takes in the IP address for which to call svcstats
+#iterates through JSON keys for the endpoints, and returns them
 
 import json
+import urllib2
 import sys
 
 def extract(arg):
+	arg = arg.strip('\n')
 	if (arg == ""):
 		return
-	parsed = json.loads(arg)
+	url = arg + ":9090/svcstats"
+	res = urllib2.urlopen(url).read()
+	parsed = json.loads(str(res))
 	for key in parsed:
-		print('"' + key + '",')
+		print('"' + arg + ':4000/' + key + '",')
 	return
 # print sys.stdin.read()
-extract(sys.stdin.read())
+extract(sys.argv[1])
--- backend/telegrafConfig.sh
+++ backend/telegrafConfig.sh
@@ -1,44 +1,28 @@
-#telegraf script
-#lookup all ips of nodes
-filename="telegraf.conf"
+#script to generate the config file for telegraf
+#lookup all ips of nodes, passes them to a python script which
+#calls svcstats, and prints out all the endpoint IPs.
+#Output is then saved to the file.
+filename="telegrafGen.conf"
 cp configBase.conf $filename
 echo "servers = [" >> $filename
-quote='"'
-comma=','
 prefix='http://'
-endpoint=':9090/svcstats'
-##array of the svcstat endpoints
-svcStats=()
+
 ##setting up the server addresses to poll from 
 for ip in $(etcdctl ls --recursive /contiv.io/service/netplugin/)
 do
-	# echo $ip
-	i="$((${#ip}-17))"
-	# j="$((${#ip}-5))"
-	ipaddr="${ip:$i:12}"
-	svcStat=($prefix$ipaddr$endpoint)
-	svcStats+=($svcStat)
-	echo $quote$svcStat$quote$comma >> $filename
+    ip="${ip##*/}"
+    ip="${ip%:*}"
+    svcStat=($prefix$ip)
+    ret=$(python containerExtract.py $svcStat)
+    echo $ret >> $filename
+
 done
 
 echo "]" >> $filename
-##setting up the tags of the containers
+
+##setting up the tags
 echo "## List of tag names to extract from top-level of JSON server response" >> $filename
 
-echo "tag_keys = [" >> $filename
-# echo ${svcStats[@]}
-for nodeSvcStat in ${svcStats[@]}
-do
-	# echo '"' >> $filename
-	curl -s $nodeSvcStat | python containerExtract.py >> $filename
-	# echo '",' >> $filename
-done
-echo ']' >> $filename
+echo 'tag_keys = ["EndpointIP","ProviderIP","ServiceIP"]' >> $filename
 
-
-## List of tag names to extract from top-level of JSON server response
-#   # tag_keys = [
-#   #   "my_tag_1",
-#   #   "my_tag_2"
-#   # ]
 
