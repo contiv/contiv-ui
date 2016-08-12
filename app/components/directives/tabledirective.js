0a05a81d368fdf9e80891673d2ccae620899ff62
Charan Shampur <cshampur@cisco.com>
2016-08-11 23:46:07 -0700

    serviceInspect/networkInspect refresh fix
    regenerating bundle.js
    
    table border fix
    
    fixing bundle.js conflict

--- app/components/directives/tabledirective.js
+++ app/components/directives/tabledirective.js
@@ -47,6 +47,7 @@ angular.module("contiv.directives")
                 var tableCtrl = this;
                 tableCtrl.chunks = [];
                 tableCtrl.pageNo = 0;
+                tableCtrl.filteredItems=[];
                 tableCtrl.sortObj=initializeSort($scope.defaultsortcolumn);
 
                 tableCtrl.size = parseInt($scope.size, 10);
