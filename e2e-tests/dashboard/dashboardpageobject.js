ad1a0c244a1da27a244519b8c14f691819fcfad2
Charan Shampur <cshampur@cisco.com>
2016-08-12 19:00:19 -0700

    corrected network policy link on dashboard page

--- e2e-tests/dashboard/dashboardpageobject.js
+++ e2e-tests/dashboard/dashboardpageobject.js
@@ -8,7 +8,7 @@ var DashboardPage = function(){
     this.networks = element(by.css("a.card[ui-sref='contiv.menu.networks.list()']"));
     this.volumes = element(by.css("a.card[ui-sref='contiv.menu.volumes.list()']"));
     this.applicationGroup = element(by.css("a.card[ui-sref='contiv.menu.applicationgroups.list()']"));
-    this.networkPolicies = element(by.css("a.card[ui-sref='contiv.menu.networkpolicies.isolation.list()']"));
+    this.networkPolicies = element.all(by.css("a.card[ui-sref='contiv.menu.networkpolicies.list.isolation()']")).get(0);
     this.storagePolicies = element(by.css("a.card[ui-sref='contiv.menu.storagepolicies.list()']"));
     this.resourceContent = element.all(by.css("div.container > div.content")).first();
     this.policyContent = element.all(by.css("div.container > div.content")).last();
--- e2e-tests/menu/menupageobject.js
+++ e2e-tests/menu/menupageobject.js
@@ -6,14 +6,14 @@
 var MenuPage = function(){
     this.dashboard = element(by.css("a.item[ui-sref='contiv.menu.dashboard()']"));
     this.applicationGroup = element(by.css("a.item[ui-sref='contiv.menu.applicationgroups.list()']"));
-    this.networkPolicies = element(by.css("a.item[ui-sref='contiv.menu.networkpolicies.isolation.list()']"));
+    this.networkPolicies = element.all(by.css("a.item[ui-sref='contiv.menu.networkpolicies.list.isolation()']")).get(0);
     this.storagePolicies = element(by.css("a.item[ui-sref='contiv.menu.storagepolicies.list()']"));
     this.serviceLb = element(by.css("a.item[ui-sref='contiv.menu.servicelbs.list()']"));
     this.networks = element(by.css("a.item[ui-sref='contiv.menu.networks.list()']"));
     this.volumes = element(by.css("a.item[ui-sref='contiv.menu.volumes.list()']"));
     this.nodes = element(by.css("a.item[ui-sref='contiv.menu.nodes.list()']"));
     this.organizations = element(by.css("a.item[ui-sref='contiv.menu.organizations.list()']"));
-    this.settings = element(by.css("a.item[ui-sref='contiv.menu.settings.details.cluster()']"));
+    this.settings = element.all(by.css("a.item[ui-sref='contiv.menu.settings.details.cluster()']")).get(0);
 }
 
 module.exports = MenuPage;
