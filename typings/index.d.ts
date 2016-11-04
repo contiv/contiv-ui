5aa466ebbb77aa8b15e766497b348084b9c30126
vishal-j <vishal.jain@gmail.com>
2016-11-04 15:05:00 -0700

    Removed duplicated core-js typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="globals/angular/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/d3/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
